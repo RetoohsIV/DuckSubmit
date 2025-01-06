763e27dce9ff91ebd0f225ebb4e20a093a6bdf0d
unknown <danielbujak2001@gmail.com>
2025-01-06 02:13:11 +0100

    Added Tutor Login

--- login.js
+++ login.js
@@ -9,6 +9,8 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     const validPassword1 = "student1";
     const validUsername2 = "student2";
     const validPassword2 = "student2";
+    const validTutor1 = "tutor1";
+    const validTutorPw1 = "tutor1";
   
     
     if ((username === validUsername1 && password === validPassword1) || (username === validUsername2 && password === validPassword2)) {
@@ -17,6 +19,12 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
       localStorage.setItem('username', username);
 
       window.location.href = 'mainpage.html';
+    } else if(username === validTutor1 && password === validTutorPw1){
+      console.log("Login successful!");
+      localStorage.setItem('isLoggedIn', 'true');
+      localStorage.setItem('username', username);
+
+      window.location.href = 'tutorpage.html';
     } else {
       alert("Invalid username or password. Please try again.");
     }
